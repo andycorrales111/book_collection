0e60c3771df513d5824778e0dec555a2a8f8df79
andycorrales111 <andycorrales@tamu.edu>
2025-02-03 18:05:49 +0000

    fixing migrations

--- db/migrate/20250202172810_add_details_to_books.rb
+++ db/migrate/20250202172810_add_details_to_books.rb
@@ -6,13 +6,15 @@
 #   end
 # end
 
-class CreateUserBooks < ActiveRecord::Migration[7.0]
+class AddDetailsToBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :title
       t.string :author
       t.decimal :price
       t.date :published_date
+      
+      t.timestamps
     end
   end
 end
